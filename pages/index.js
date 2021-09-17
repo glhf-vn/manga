311cb99f98a802361694af904d235d62a3aca3df
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2021-09-18 01:43:24 +0700

    update banner

--- pages/index.js
+++ pages/index.js
@@ -332,13 +332,8 @@ export default function Home({ update, info }) {
               ]}
               eventClick={openDetailedModal}
             />
-<<<<<<< HEAD
             <div className="uk-margin-top" style={{ border: '1px solid #e5e5e5' }}>
               <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FmangaGLHF&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="340" height="500" style={{border: "none", overflow: "hidden"}} scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-=======
-            <div className="uk-margin-top">
-              <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FmangaGLHF&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="340" height="500" style={{border: "none", overflow: "hidden"}} scrolling="no" frameBorder="0" allowFullScreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
->>>>>>> de8a8907faa158df79e02e0747428ca935f8e44e
             </div>
           </div>
         </div>
