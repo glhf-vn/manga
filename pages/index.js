4effe4058c8d8fddb13e824a106c1fb4b428a292
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2022-04-16 23:36:17 +0700

    Make listView primary on mobile

--- pages/index.js
+++ pages/index.js
@@ -185,10 +185,6 @@ export default function Home({ update, info }) {
               plugins={[dayGridPlugin, googleCalendarPlugin]}
               locale='vi'
               initialView='dayGridMonth'
-              titleFormat={{
-                year: 'numeric',
-                month: 'numeric'
-              }}
               dayHeaders={false}
               aspectRatio={0}
               firstDay={1}
@@ -332,7 +328,11 @@ export default function Home({ update, info }) {
               plugins={[listPlugin, googleCalendarPlugin]}
               locale='vi'
               initialView='listMonth'
-              headerToolbar='false'
+              height="80vh"
+              titleFormat={{
+                year: 'numeric',
+                month: 'numeric'
+              }}
               eventSources={[
                 {
                   googleCalendarApiKey: "AIzaSyD5LFxB0haFZegifWlSn-S3ebkK1vfiM3U",
@@ -397,7 +397,7 @@ export default function Home({ update, info }) {
               ]}
               eventClick={openDetailedModal}
             />
-            <div className="uk-margin-top" style={{ border: '1px solid #e5e5e5' }}>
+            <div className={`uk-margin-top ${styles.hideOnMobile}`} style={{ border: '1px solid #e5e5e5' }}>
               <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FmangaGLHF&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="340" height="500" style={{ border: "none", overflow: "hidden" }} scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
             </div>
           </div>
