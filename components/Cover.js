7537392052081f8b9031068352a79635de2ec439
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2022-10-22 10:53:19 +0700

    fixed compatibility on safari

--- components/Cover.js
+++ components/Cover.js
@@ -1,6 +1,6 @@
 export default function Cover({ name, id, isBanner = false }) {
   return (
-    <div className="cursor-default drop-shadow-lg">
+    <div className="cursor-default shadow-md hover:shadow-lg h-fit rounded-2xl">
       <object
         className="rounded-2xl w-full h-auto"
         data={`https://res.cloudinary.com/glhfvn/image/upload/c_fit/v1/covers/${id}.jpg`}
