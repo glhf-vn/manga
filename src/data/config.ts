338230e41409fbc8293130a886176c93d9e6ef2b
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-01-24 23:35:26 +0700

    removed url field from og meta tag

--- src/data/config.ts
+++ src/data/config.ts
@@ -7,7 +7,6 @@ export const seoConfig: DefaultSeoProps = {
   openGraph: {
     type: "website",
     locale: "vi_VN",
-    url: "https://manga.glhf.vn/",
     siteName: "mangaGLHF",
   },
   twitter: {
