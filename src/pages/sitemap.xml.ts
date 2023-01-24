4aa4f25a1faab2043289324aae14404260f43cca
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-01-25 00:19:07 +0700

    added sitemap generation

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,60 @@
+// ref: https://nextjs.org/learn/seo/crawling-and-indexing/xml-sitemaps
+
+import type { GetServerSideProps } from "next";
+
+import { Serie } from "@data/public.types";
+
+const SERIES_DATA = "https://manga.glhf.vn/api/series";
+const SERIES_URL = "https://manga.glhf.vn/license";
+
+function generateSiteMap(series: Serie[]) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>https://manga.glhf.vn</loc>
+     </url>
+     <url>
+       <loc>https://manga.glhf.vn/donate</loc>
+     </url>
+     <url>
+       <loc>https://manga.glhf.vn/listing</loc>
+     </url>
+     <url>
+       <loc>https://manga.glhf.vn/license</loc>
+     </url>
+     ${series
+       .map(({ id }) => {
+         return `
+       <url>
+           <loc>${`${SERIES_URL}/${id}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  // We make an API call to gather the URLs for our site
+  const request = await fetch(SERIES_DATA);
+  const series: Serie[] = await request.json();
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(series);
+
+  res.setHeader("Content-Type", "text/xml");
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default SiteMap;
