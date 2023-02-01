f3fa1560d71939ff30a309a55259d5e12d88a9ec
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-02-01 23:29:00 +0700

    changed `tiki` and `fahasa` apis to edge functions

--- src/pages/api/fahasa.ts
+++ src/pages/api/fahasa.ts
@@ -1,17 +1,20 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import type { FahasaResponse, ListingResponse } from "@data/api.types";
+import { NextRequest, NextResponse } from "next/server";
+import type { FahasaResponse } from "@data/api.types";
 
-export default async function Fahasa(
-  req: NextApiRequest,
-  res: NextApiResponse<ListingResponse[]>
-) {
-  const {
-    query: { category, page },
-    method,
-  } = req;
+export const config = {
+  runtime: "edge",
+};
+
+export default async function Fahasa(req: NextRequest) {
+  const { method } = req;
 
   switch (method) {
     case "GET":
+      const { searchParams } = new URL(req.url);
+
+      const category = searchParams.get("category");
+      const page = searchParams.get("page");
+
       const fahasaResponse = await fetch(
         `https://fahasa.com/fahasa_catalog/product/loadproducts?category_id=${category}&currentPage=${page}&limit=24&order=created_at&series_type=0`,
         {
@@ -25,26 +28,35 @@ export default async function Fahasa(
 
       const { product_list }: FahasaResponse = await fahasaResponse.json();
 
-      if (product_list) {
+      if (product_list && product_list.length > 0) {
         // Get data from your database
-        res.status(200).json(
-          product_list.map((product) => ({
-            id: product.product_id,
-            name: product.product_name,
-            image: product.image_src,
-            url: product.product_url,
-            price: product.product_finalprice * 1000,
-            original_price: product.product_price * 1000,
-            discount: product.discount,
-          }))
+        return new NextResponse(
+          JSON.stringify(
+            product_list.map((product) => ({
+              id: product.product_id,
+              name: product.product_name,
+              image: product.image_src,
+              url: product.product_url,
+              price: product.product_finalprice * 1000,
+              original_price: product.product_price * 1000,
+              discount: product.discount,
+            }))
+          ),
+          {
+            status: 200,
+            headers: {
+              "content-type": "application/json",
+            },
+          }
         );
       } else {
-        res.status(204).end();
+        return new NextResponse(null, {
+          status: 204,
+        });
       }
-
-      break;
     default:
-      res.setHeader("Allow", ["GET"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
+      return new NextResponse(undefined, {
+        status: 405,
+      });
   }
 }
--- src/pages/api/tiki.ts
+++ src/pages/api/tiki.ts
@@ -1,17 +1,20 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import type { TikiResponse, ListingResponse } from "@data/api.types";
+import { NextRequest, NextResponse } from "next/server";
+import type { TikiResponse } from "@data/api.types";
 
-export default async function Tiki(
-  req: NextApiRequest,
-  res: NextApiResponse<ListingResponse[]>
-) {
-  const {
-    query: { category, page },
-    method,
-  } = req;
+export const config = {
+  runtime: "edge",
+};
+
+export default async function Tiki(req: NextRequest) {
+  const { method } = req;
 
   switch (method) {
     case "GET":
+      const { searchParams } = new URL(req.url);
+
+      const category = searchParams.get("category");
+      const page = searchParams.get("page");
+
       const tikiResponse = await fetch(
         `https://tiki.vn/api/personalish/v1/blocks/listings?limit=24&category=${category}&page=${page}&sort=newest&seller=1`,
         {
@@ -25,28 +28,37 @@ export default async function Tiki(
 
       const { data }: TikiResponse = await tikiResponse.json();
 
-      if (data) {
+      if (data && data.length > 0) {
         // Get data from your database
-        res.status(200).json(
-          data.map((product) => ({
-            id: product.id,
-            name: product.name,
-            image: product.thumbnail_url,
-            url: `https://tiki.vn/${product.url_path}`,
-            price: product.price,
-            original_price: product.original_price,
-            discount: Math.round(
-              (product.discount / product.original_price) * 100
-            ),
-          }))
+        return new NextResponse(
+          JSON.stringify(
+            data.map((product) => ({
+              id: product.id,
+              name: product.name,
+              image: product.thumbnail_url,
+              url: `https://tiki.vn/${product.url_path}`,
+              price: product.price,
+              original_price: product.original_price,
+              discount: Math.round(
+                (product.discount / product.original_price) * 100
+              ),
+            }))
+          ),
+          {
+            status: 200,
+            headers: {
+              "content-type": "application/json",
+            },
+          }
         );
       } else {
-        res.status(204).end();
+        return new NextResponse(null, {
+          status: 204,
+        });
       }
-
-      break;
     default:
-      res.setHeader("Allow", ["GET"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
+      return new NextResponse(undefined, {
+        status: 405,
+      });
   }
 }
