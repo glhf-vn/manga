c9a9c8335ce9d484a8af068714ef2653a0901535
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-02-02 16:50:28 +0700

    added new `MonthSelect` behavior

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,10 +14,10 @@ import { VND } from "@data/config";
 import { getEntries, getPublishers } from "@lib/supabase";
 
 import { useEffect, useState } from "react";
-import { DateTime } from "luxon";
+import { DateTime, type MonthNumbers } from "luxon";
 import useSWR from "swr";
 
-import { Dialog, Menu, Transition } from "@headlessui/react";
+import { Dialog, Popover, Listbox, Transition } from "@headlessui/react";
 import { NextSeo } from "next-seo";
 import {
   BsBoxArrowUpRight,
@@ -130,22 +130,26 @@ const Slider = ({ data }: SliderProps) => {
 };
 
 const MonthSelect = ({ date, options }: PaginationProps) => {
-  const { month } = date;
+  const currentYear = DateTime.now().year;
+  const { month, year } = date;
   const { changeDate } = options;
 
-  const thisMonth = DateTime.now();
-  const prevMonth = thisMonth.minus({ month: 1 });
-  const nextMonth = thisMonth.plus({ month: 1 });
+  const [selectedYear, changeSelectedYear] = useState(year);
+  const [selectedMonth, changeSelectedMonth] = useState(month);
+
+  const availableYear = [
+    ...Array.from(
+      { length: currentYear - 2021 + 2 }, // get from 2021 + the year after that
+      (_, index) => 2021 + index
+    ),
+  ];
 
   return (
-    <Menu
-      as="div"
-      className="relative inline-block font-kanit text-2xl font-bold"
-    >
-      <Menu.Button className="flex items-center gap-3 rounded-2xl bg-zinc-200 py-1 px-2 dark:bg-zinc-700">
-        tháng {month}
+    <Popover className="relative inline-block">
+      <Popover.Button className="flex items-center gap-3 rounded-2xl bg-zinc-200 py-1 px-2 text-2xl font-bold transition-all duration-150 ease-linear hover:bg-zinc-300 dark:bg-zinc-700 dark:hover:bg-zinc-600">
+        {month}/{year}
         <BsChevronDown className="text-sm" />
-      </Menu.Button>
+      </Popover.Button>
       <Transition
         enter="transition duration-100 ease-out"
         enterFrom="transform scale-95 opacity-0"
@@ -154,31 +158,76 @@ const MonthSelect = ({ date, options }: PaginationProps) => {
         leaveFrom="transform scale-100 opacity-100"
         leaveTo="transform scale-95 opacity-0"
       >
-        <Menu.Items className="absolute right-0 mt-3 w-full overflow-hidden rounded-2xl bg-zinc-200 shadow-lg dark:bg-zinc-700">
-          <Menu.Item
-            as="div"
-            onClick={() => changeDate(prevMonth)}
-            className="transition-color block cursor-pointer py-1 px-2 duration-75 ease-linear ui-active:bg-zinc-300 ui-active:dark:bg-zinc-600"
+        <Popover.Panel className="absolute mt-6 w-screen max-w-xs space-y-3 rounded-2xl bg-zinc-100 p-3 pb-2 shadow-lg dark:bg-zinc-700">
+          <Listbox value={selectedYear} onChange={changeSelectedYear}>
+            <Listbox.Button className="relative w-full rounded-xl bg-zinc-200 py-1 px-2 text-lg font-bold transition-all duration-300 hover:bg-zinc-300 dark:bg-zinc-600 dark:hover:bg-zinc-500">
+              <span>{selectedYear}</span>
+              <span className="absolute inset-y-0 right-2 flex items-center">
+                <BsChevronDown className="text-sm" />
+              </span>
+            </Listbox.Button>
+            <Transition
+              enter="transition ease-out duration-100"
+              enterFrom="opacity-0 -translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Listbox.Options className="absolute inset-x-0 max-h-40 overflow-auto rounded-xl bg-zinc-200 dark:bg-zinc-600">
+                {availableYear.map((year) => (
+                  <Listbox.Option
+                    key={year}
+                    value={year}
+                    className={({ active }) =>
+                      `w-full cursor-default py-1 px-2 text-center ${
+                        active ? "bg-zinc-300 dark:bg-zinc-500" : "bg-inherit"
+                      }`
+                    }
+                  >
+                    {({ selected }) => (
+                      <span className={`${selected && "font-bold"}`}>
+                        {year}
+                      </span>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </Listbox>
+          <div className="grid grid-cols-3 gap-1">
+            {[...Array(12)].map((_, i) => (
+              <Button
+                key={i}
+                hoverable={false}
+                intent={selectedMonth === i + 1 ? "primary" : "none"}
+                onClick={() => changeSelectedMonth((i + 1) as MonthNumbers)}
+                className={
+                  selectedMonth !== i + 1
+                    ? `hover:bg-zinc-200 dark:hover:bg-zinc-600`
+                    : ""
+                }
+              >
+                Tháng {i + 1}
+              </Button>
+            ))}
+          </div>
+          <Button
+            intent="secondary"
+            hoverable={false}
+            className="w-full dark:bg-zinc-600 dark:hover:bg-zinc-500"
+            onClick={() =>
+              changeDate({
+                year: selectedYear,
+                month: selectedMonth,
+              })
+            }
           >
-            {prevMonth.toFormat("MMMM")}
-          </Menu.Item>
-          <Menu.Item
-            as="div"
-            onClick={() => changeDate(thisMonth)}
-            className="transition-color block cursor-pointer py-1 px-2 duration-75 ease-linear ui-active:bg-zinc-300 ui-active:dark:bg-zinc-600"
-          >
-            {thisMonth.toFormat("MMMM")}
-          </Menu.Item>
-          <Menu.Item
-            as="div"
-            onClick={() => changeDate(nextMonth)}
-            className="transition-color block cursor-pointer py-1 px-2 duration-75 ease-linear ui-active:bg-zinc-300 ui-active:dark:bg-zinc-600"
-          >
-            {nextMonth.toFormat("MMMM")}
-          </Menu.Item>
-        </Menu.Items>
+            Chọn
+          </Button>
+        </Popover.Panel>
       </Transition>
-    </Menu>
+    </Popover>
   );
 };
 
@@ -245,6 +294,7 @@ const FilterModal = ({
           <div className="grid gap-x-3 gap-y-1 sm:grid-cols-2">
             <Button
               intent="secondary"
+              hoverable={false}
               className="mb-3"
               onClick={() =>
                 setCurrentValues([...values.map((value) => value.id)])
@@ -255,6 +305,7 @@ const FilterModal = ({
             <Button
               intent="none"
               className="mb-3"
+              hoverable={false}
               onClick={() => setCurrentValues([])}
             >
               Bỏ chọn tất cả
